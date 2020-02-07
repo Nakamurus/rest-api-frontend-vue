3828c6a0b0945ee9d7ca48d5de62b331000b4d3f
nakamurusun <3872mn@gmail.com>
2020-02-07 23:11:00 +0900

    added vee validate

--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
+import { store } from './store'
+import { ValidationProvider, extend, ValidationObserver } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules'
+
+extend('required', {
+    ...required,
+    message: 'This field is required'
+})
+
+extend('min', {
+    validate(value, args) {
+        return value.length >= args.length;
+    },
+    params: ['length'],
+    message: 'The {_field_} field must have at least {min} characters'
+})
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 
 Vue.config.productionTip = false
 
 new Vue({
+  el:'#app',
   router,
   store,
   render: h => h(App)
